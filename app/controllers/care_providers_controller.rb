c17f38a85bdce72fe2a7ea3dffb34877f80deffc
Aadil Bhatti <aadilzbhatti@gmail.com>
2015-09-16 01:00:27 -0500

    lala

--- app/controllers/care_providers_controller.rb
+++ app/controllers/care_providers_controller.rb
@@ -69,6 +69,6 @@ class CareProvidersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def care_provider_params
-      params.require(:care_provider).permit(:name, :email)
+      params.require(:care_provider).permit(:name, :email, :password, :password_confirmation)
     end
 end
